a21dad22b4300e66bfbf6066e66fd1704265fa86
rosi929 <140379049+rosi929@users.noreply.github.com>
2023-07-24 09:29:13 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,17 @@
+
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+
+public class BasicFrameApp {
+
+   
+    public static void main(String[] args) {
+       
+        MyFirstFrame myFrame;
+        
+        myFrame= new MyFirstFrame();
+    }
+    
+}
